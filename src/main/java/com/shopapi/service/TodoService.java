e0ec97dfa4e9c0a7c2e58a52a1239ca482b6c500
Wonjung <gimpo5975@naver.com>
2024-04-30 09:28:24 +0900

    TodoService 추상메소드(CRUD) 작성

--- src/main/java/com/shopapi/service/TodoService.java
+++ src/main/java/com/shopapi/service/TodoService.java
@@ -7,5 +7,15 @@ public interface TodoService {
 //#1. 등록 기능
     Long register(TodoDTO todoDTO);
 
+    //#2. 조회
+    TodoDTO get(Long tno);
+    
+    //#3. 업데이트
+    void modify(TodoDTO todoDTO);
+
+    //#4. 삭제
+    void remove(Long tno);
+
+    //#5. 페이징
 
 }//end of interface
