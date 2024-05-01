3a001c53cece1087bdebcfcc54c48bcce0bf208f
Wonjung <gimpo5975@naver.com>
2024-05-01 09:26:02 +0900

    TodoServiceImpl 수정

--- src/main/java/com/shopapi/service/TodoServiceImpl.java
+++ src/main/java/com/shopapi/service/TodoServiceImpl.java
@@ -1,6 +1,5 @@
 package com.shopapi.service;
 
-
 import com.shopapi.domain.Todo;
 import com.shopapi.dto.PageRequestDTO;
 import com.shopapi.dto.PageResponseDTO;
@@ -9,16 +8,22 @@ import com.shopapi.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 @Log4j2
 @RequiredArgsConstructor  //생성자 자동 주입
-public class TodoServiceImpl implements TodoService{
+public class TodoServiceImpl implements TodoService {
 
     //자동 주입 대상은 final
     private final ModelMapper modelMapper;
@@ -45,10 +50,12 @@ public class TodoServiceImpl implements TodoService{
     public TodoDTO get(Long tno) {
 
         Optional<Todo> result = todoRepository.findById(tno);
-        //Optional : 래퍼 클래스
-        //  이것을 사용하지 않으면 매번 if 문을 이용해 null 인지 체크한 후 사용
-        //  이것을 이용하면 null 체크를 위한 if 문 없이도 NullPointerException 이 발생하지 않음
-        
+        //Optional :래퍼 클래스
+        //          이것을 사용하지 않으면 매번 if문을 이욯새 null인지 체크한 후 사용
+        //          이것을 이용하면 null 체크를 위한 if문 없이도 NullPointerException이 발생하지
+        //          않음
+
+
         Todo todo = result.orElseThrow();
 
         TodoDTO dto = modelMapper.map(todo, TodoDTO.class);
@@ -80,14 +87,35 @@ public class TodoServiceImpl implements TodoService{
 
     }
 
+    //#5. 페이징 처리
     @Override
     public PageResponseDTO<TodoDTO> list(PageRequestDTO pageRequestDTO) {
-        return null;
+
+        Pageable pageable =
+                PageRequest.of(
+                        pageRequestDTO.getPage() - 1,
+                        pageRequestDTO.getSize(),
+                        Sort.by("tno").descending());
+
+        Page<Todo> result = todoRepository.findAll(pageable);
+
+        List<TodoDTO> dtoList = result.getContent().stream()
+                .map(todo -> modelMapper.map(todo, TodoDTO.class))
+                .collect(Collectors.toList());
+
+        long totalCount = result.getTotalElements();
+
+        PageResponseDTO<TodoDTO> responseDTO = PageResponseDTO.<TodoDTO>withAll()
+                .dtoList(dtoList)
+                .pageRequestDTO(pageRequestDTO)
+                .totalCount(totalCount)
+                .build();
+
+        return responseDTO;
     }
 
     @Override
     public PageResponseDTO<TodoDTO> getList(PageRequestDTO pageRequestDTO) {
         return null;
     }
-
 }
