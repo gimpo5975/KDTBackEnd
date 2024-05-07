425a6678b2379fb63e10e4ab157954aa337751ec
Wonjung <gimpo5975@naver.com>
2024-05-07 09:46:24 +0900

    프로젝트 리셋

--- src/main/java/com/shopapi/repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shopapi.repository;
-
-import com.shopapi.domain.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long>{
-
-}
--- src/main/java/com/shopapi/repository/search/TodoSearchImpl.java
+++
@@ -1,2 +0,0 @@
-package com.shopapi.repository.search;
-
