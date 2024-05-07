438a9f0f2d374fe7c040dc04f1f8194cca46b3e1
Wonjung <gimpo5975@naver.com>
2024-05-07 16:31:59 +0900

    Todo entity 작성

--- src/main/java/com/shopapi/domain/Todo.java
+++ src/main/java/com/shopapi/domain/Todo.java
@@ -1,4 +1,32 @@
 package com.shopapi.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@ToString
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Todo {
-}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tno;
+
+    private String title;
+
+    private  String content;
+
+    private boolean complete;
+
+    private LocalDate deuDate;
+
+}//end of class
