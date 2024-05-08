082bb0dddf6e4ee79ce0b301e5c8ce7a59454f53
Wonjung <gimpo5975@naver.com>
2024-05-08 16:33:07 +0900

    entity -> dto로 변경

--- src/main/java/com/shopapi/service/TodoServiceImpl.java
+++ src/main/java/com/shopapi/service/TodoServiceImpl.java
@@ -1,12 +1,29 @@
 package com.shopapi.service;
 
+import com.shopapi.domain.Todo;
+import com.shopapi.dto.TodoDTO;
+import com.shopapi.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Log4j2
-public class TodoServiceImpl {
+@RequiredArgsConstructor
+public class TodoServiceImpl implements TodoService {
 
+    private final TodoRepository todoRepository;
 
+    @Override
+    public TodoDTO get(Long tno) {
 
+        Optional<Todo> result = todoRepository.findById(tno);
+
+        Todo todo  = result.orElseThrow();
+
+        return entityToDTO(todo);
+    }
 }
