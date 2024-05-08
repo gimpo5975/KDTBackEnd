df5f990be4ac9b98a29febf61b9b819b2ce4f641
Wonjung <gimpo5975@naver.com>
2024-05-08 12:43:16 +0900

    코드설명

--- src/main/java/com/shopapi/dto/TodoDTO.java
+++ src/main/java/com/shopapi/dto/TodoDTO.java
@@ -1,4 +1,20 @@
 package com.shopapi.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class TodoDTO {
+
+    private Long tno;
+
+    private String title;
+
+    private  String content;
+
+    private boolean complete;
+
+    private LocalDate deuDate;
+
 }
--- src/main/java/com/shopapi/repository/search/TodoSearchImpl.java
+++ src/main/java/com/shopapi/repository/search/TodoSearchImpl.java
@@ -23,18 +23,33 @@ public class TodoSearchImpl extends QuerydslRepositorySupport implements TodoSea
         log.info("search1.........................");
 
         QTodo todo = QTodo.todo;
+        //QTodo는 Todo 엔티티의 QueryDSL을 위한 쿼리 타입입니다.
+        //todo는 QTodo.todo에서 Todo 엔티티의 인스턴스를 가져와 생성한 변수입니다.
 
         JPQLQuery<Todo> query = from(todo);
+        //from(todo)는 Todo 엔티티에 대한 JPQLQuery를 생성합니다. 이 쿼리 객체는 Todo 엔티티에 대한
+        // 쿼리를 작성할 수 있게 합니다.
+        //이 쿼리는 Todo 엔티티의 인스턴스(todo)를 대상으로 합니다.
 
         query.where(todo.title.contains("1"));
+        //query.where()는 쿼리에 조건을 추가합니다.
+        //todo.title.contains("1")는 Todo 엔티티의 title 필드가 "1"을 포함하는지 확인하는 조건을 추가합니다.
 
         Pageable pageable = PageRequest.of(1, 10, Sort.by("tno").descending());
+        //Pageable은 페이지네이션을 위한 설정을 정의하는 인터페이스입니다.
+        //PageRequest.of(1, 10, Sort.by("tno").descending())는 페이지 번호(0부터 시작하므로 1은 두 번째 페이지),
+        // 페이지 크기(10개 항목), 정렬 조건(키 필드인 tno를 내림차순)으로 페이지네이션 설정을 생성합니다.
+
 
         this.getQuerydsl().applyPagination(pageable, query);
+        //getQuerydsl().applyPagination(pageable, query)는 쿼리에 페이지네이션 설정을 적용합니다.
+        //pageable 매개변수는 페이지네이션 설정을, query 매개변수는 쿼리를 나타냅니다.
 
         query.fetch();// 목록 데이터
+        //query.fetch()는 쿼리를 실행하고 결과를 가져옵니다.
+        //이 호출은 쿼리 결과 목록을 반환하지만, 결과는 여기서 활용하지 않습니다.
 
-        query.fetchCount();
+        query.fetchCount();//query.fetchCount()는 쿼리 결과의 총 항목 수를 반환합니다.
 
         return null;
     }
