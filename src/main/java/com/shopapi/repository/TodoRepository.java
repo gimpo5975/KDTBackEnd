3dcf1f86724863464ba2c1ec33251c7daf2f4246
Wonjung <gimpo5975@naver.com>
2024-05-08 10:19:27 +0900

    TodoRepository 생성

---
+++ src/main/java/com/shopapi/repository/TodoRepository.java
@@ -0,0 +1,7 @@
+package com.shopapi.repository;
+
+import com.shopapi.domain.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
