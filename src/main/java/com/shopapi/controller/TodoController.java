f35939a7a5e6cda212f81f2763a94fbae3fad574
Wonjung <gimpo5975@naver.com>
2024-05-10 09:39:09 +0900

    등록 PostMapping

--- src/main/java/com/shopapi/controller/TodoController.java
+++ src/main/java/com/shopapi/controller/TodoController.java
@@ -6,10 +6,9 @@ import com.shopapi.dto.TodoDTO;
 import com.shopapi.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -30,6 +29,15 @@ public class TodoController {
         log.info("list.............."+ pageRequestDTO);
 
         return todoService.getList(pageRequestDTO);
+    }
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDTO todoDTO){
+        log.info("todoDTO : " + todoDTO);
+
+        Long tno = todoService.register(todoDTO);
+
+        return Map.of("TNO", tno);
     }
 
 }
