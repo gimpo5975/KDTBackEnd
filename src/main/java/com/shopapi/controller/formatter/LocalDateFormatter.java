e99515497e94e1fa25ce6edf503015f9fac103e0
Wonjung <gimpo5975@naver.com>
2024-05-10 10:04:07 +0900

    텍스트 -> 날짜 맵핑 / 날짜 -> 객체 맵핑

--- src/main/java/com/shopapi/controller/formatter/LocalDateFormatter.java
+++ src/main/java/com/shopapi/controller/formatter/LocalDateFormatter.java
@@ -11,10 +11,10 @@ public class LocalDateFormatter implements Formatter<LocalDate> {
     @Override
     public LocalDate parse(String text, Locale locale) throws ParseException {
         return LocalDate.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-    }
+    }//문자열로 들어온 날짜 데이터를 원하는 데이트타입으로 포맷
 
     @Override
     public String print(LocalDate object, Locale locale) {
         return DateTimeFormatter.ofPattern("yyyy-MM-dd").format(object);
-    }
+    }//데이트타입 날짜를 객체로 변환
 }
