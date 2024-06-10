08be57d36c47018189f113f2ad7e2f31e907d452
Wonjung <gimpo5975@naver.com>
2024-06-10 11:30:10 +0900

    file등록 util 구현

--- src/main/java/com/shopapi/util/CustomFileUtil.java
+++ src/main/java/com/shopapi/util/CustomFileUtil.java
@@ -8,7 +8,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Component
 @Log4j2
@@ -32,7 +38,29 @@ public class CustomFileUtil {
     }
 
     public List<String> saveFiles(List<MultipartFile> files) throws RuntimeException{
-        return null;
+
+        if(files == null || files.size() == 0){
+            return null;
+        }
+
+        List<String> uploadNames = new ArrayList<>();
+
+        for(MultipartFile file:files){
+
+            String savedName = UUID.randomUUID().toString()+"_"+file.getOriginalFilename();
+
+            Path savePath = Paths.get(uploadPath, savedName);
+
+            try {
+                Files.copy(file.getInputStream(), savePath);
+                uploadNames.add(savedName);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }//end of try-catch
+
+        }//end of for
+
+        return uploadNames;
     }
 
 }
